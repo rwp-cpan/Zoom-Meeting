536e1b4d11f3fe419d19681a0b1ce60d5b3cd26d
rwp0 <rwp.primary@gmail.com>
2023-06-01 21:30:31 +0200

    Add launch method
    Launch Zoom thorugh WSL

--- bin/zoom.pl
+++ bin/zoom.pl
@@ -2,6 +2,8 @@ use v5.37.12;
 
 use Zoom::Meeting;
 
-my $zoom = Zoom::Meeting -> new (id => '123456789', password => 'pass');
+my $zoom = Zoom::Meeting -> new (id => '', password => '');
 
 say $zoom;
+
+$zoom -> launch;
--- lib/Zoom/Meeting.pm
+++ lib/Zoom/Meeting.pm
@@ -1,27 +1,44 @@
 use v5.37.12;
-use experimental qw(class);
+use experimental qw( class try builtin );
+use builtin qw( true false blessed );
 
 package Zoom::Meeting; # quirk
 class Zoom::Meeting {
 
   use overload q("") => sub { $_[0] -> _url; };
+  use Path::Tiny;
+  use System::Command;
 
+  # @formatter:off
   field $id :param;
   field $password :param = undef;
+  # @formatter:on
 
-  method _url () {
+  method _url ( ) {
     use URI;
-    my $uri = URI -> new ('zoommtg://zoom.us'); # scheme is '_foreign' class, so no 'host' method
-    $uri -> path('join');
+    my $uri = URI -> new( 'zoommtg://zoom.us' ); # scheme is '_foreign' class, so no 'host' method
+    $uri -> path( 'join' );
     $uri -> query_form_hash( {
-      'confno' => $id,
-      'pwd' => $password
+      'confno' => $id ,
+      'pwd'    => $password
     } );
     return $uri;
   }
 
-  method launch () {
-    say $self -> _url;
+  method launch ( ) {
+
+    my $zoom;
+
+    if ( $^O eq 'linux' and -f '/proc/sys/fs/binfmt_misc/WSLInterop' ) {
+      $zoom = path '/mnt/c/Program Files/Zoom/bin/zoom.exe';
+    }
+
+    System::Command -> new(
+      $zoom ,
+      "--url=@{ [ $self -> _url ] }" ,
+      # { trace => 3 }
+    );
+
   }
 
 }
