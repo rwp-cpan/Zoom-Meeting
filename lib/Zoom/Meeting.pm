0a40a0ebeab252308d20172337fcd1ada47c17de
rwp0 <rwp.primary@gmail.com>
2023-06-01 21:37:30 +0200

    Move URI up

--- lib/Zoom/Meeting.pm
+++ lib/Zoom/Meeting.pm
@@ -6,6 +6,7 @@ package Zoom::Meeting; # quirk
 class Zoom::Meeting {
 
   use overload q("") => sub { $_[0] -> _url; };
+  use URI;
   use Path::Tiny;
   use System::Command;
 
@@ -15,7 +16,6 @@ class Zoom::Meeting {
   # @formatter:on
 
   method _url ( ) {
-    use URI;
     my $uri = URI -> new( 'zoommtg://zoom.us' ); # scheme is '_foreign' class, so no 'host' method
     $uri -> path( 'join' );
     $uri -> query_form_hash( {
